33d2ae52207ef53f0492474def774026e364c211
pangelu9 <80212723+pangelu9@users.noreply.github.com>
2021-03-21 17:48:35 +0200

    resolved cuda run error pointed out by ella+ben

--- optimization/loss.py
+++ optimization/loss.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 from util.distributions import log_normal_dist
 
-def binary_loss_function(x_recon, x, z_mu, z_var, z_0, z_k, log_det_jacobians, z_size, beta=1, summ = True):
+def binary_loss_function(x_recon, x, z_mu, z_var, z_0, z_k, log_det_jacobians, z_size, cuda, beta=1, summ = True):
     """
     x_recon: shape: (batch_size, num_channels, pixel_width, pixel_height), i.e. for MNIST [batch_size, 1, 28, 28]
     x: shape (batchsize, num_channels, pixel_width, pixel_height), pixel values rescaled between [0, 1].
@@ -22,8 +22,12 @@ def binary_loss_function(x_recon, x, z_mu, z_var, z_0, z_k, log_det_jacobians, z
         reconstruction_loss = nn.BCELoss(reduction='sum')
         log_p_xz = reconstruction_loss(x_recon, x)  #log_p(x|z_k)
         
+        logvar=torch.zeros(batch_size, z_size) 
         
-        log_p_zk = log_normal_dist(z_k, mean=0, logvar=torch.zeros(batch_size, z_size), dim=1) # ln p(z_k) = N(0,I) 
+        if cuda == True:                      
+            logvar=logvar.cuda()
+        
+        log_p_zk = log_normal_dist(z_k, mean=0, logvar=logvar, dim=1) # ln p(z_k) = N(0,I) 
         log_q_z0 = log_normal_dist(z_0, mean=z_mu, logvar=z_var.log(), dim=1)
   
         kl = torch.sum(log_q_z0 - beta * log_p_zk) - torch.sum(log_det_jacobians) #sum over batches
@@ -44,7 +48,11 @@ def binary_loss_function(x_recon, x, z_mu, z_var, z_0, z_k, log_det_jacobians, z
         log_p_xz = reconstruction_loss(x_recon.view(batch_size, -1), x.view(batch_size, -1))  #log_p(x|z_k)
         log_p_xz = torch.sum(log_p_xz, dim=1)
         
-        log_p_zk = log_normal_dist(z_k, mean=0, logvar=torch.zeros(batch_size, z_size), dim=1)
+        logvar=torch.zeros(batch_size, z_size)  
+        if cuda == True:                       
+            logvar=logvar.cuda()
+        
+        log_p_zk = log_normal_dist(z_k, mean=0, logvar=logvar, dim=1)
         log_q_z0 = log_normal_dist(z_0, mean=z_mu, logvar=z_var.log(), dim=1)
         #Equation (20)
         elbo = log_q_z0 - beta * (log_p_zk - log_p_xz) - log_det_jacobians  
