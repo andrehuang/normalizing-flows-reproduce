4e8af2de0eb7310d4271acc881065809111e80c6
Angeliki Papathanasiou <80212723+pangelu9@users.noreply.github.com>
2021-04-06 22:59:47 +0300

    added sylvester orthogonal flow

--- models/VAE.py
+++ models/VAE.py
@@ -356,3 +356,148 @@ class NICEVAE_amor(VAE):
         x_mean = self.decode(z[-1])
 
         return x_mean, z_mu, z_var, self.log_det_j, z[0], z[-1]
+   
+class Sylvester_ortho_VAE(VAE):
+    """
+    Variational auto-encoder with orthogonal flows in the encoder.
+    """
+
+    def __init__(self, encoder, decoder, args):
+        super(Sylvester_ortho_VAE, self).__init__(encoder, decoder, args)
+
+        # Flow parameters
+        flow = flows.Sylvester
+        self.num_flows = args.num_flows
+        self.M = args.num_orthonormal_vec
+    
+        if (self.M > self.z_size) or (self.M <= 0): #z_size: number of stochastic hidden units
+            raise ValueError('ERROR: The number of orthonomal vectors (M) must be positive and smaller than the dimension of z (D)')
+        
+        # Orthogonalization parameters
+        self.epsilon = 1.e-6
+        self.steps = 100
+        self.invertibility = args.syl_ortho_invertible
+        
+        self.identity = torch.eye(self.M, self.M)
+        self.identity = self.identity.unsqueeze(0)
+        
+        # torch used to make R and R_tilde upper triangluar 
+        self.upper_triang = torch.triu(torch.ones(self.M, self.M))
+        self.upper_triang = self.upper_triang.unsqueeze(0).unsqueeze(3)
+        
+        if self.is_cuda:
+            self.identity = self.identity.cuda()
+            self.upper_triang =  self.upper_triang.cuda()
+                
+        # Amortized flow parameters
+        self.amor_Q = nn.Linear(self.encoder_dim, self.num_flows * self.z_size * self.M)
+        self.amor_b = nn.Linear(self.encoder_dim, self.num_flows * self.M)
+        self.amor_R = nn.Linear(self.encoder_dim, self.num_flows * self.M * self.M)
+        self.amor_R_tilde = nn.Linear(self.encoder_dim, self.num_flows * self.M * self.M)
+               
+        #for the flow to be invertible we need R*R_tilde>-1 - default: self.invertibility = False
+        if self.invertibility:
+            self.amor_diag1 = nn.Sequential(nn.Linear(self.encoder_dim, self.num_flows * self.M), nn.Tanh())
+            self.amor_diag2 = nn.Sequential(nn.Linear(self.encoder_dim, self.num_flows * self.M), nn.Tanh())
+        
+        # Normalizing flow layers
+        for k in range(self.num_flows):
+            flow_k = flow(self.M)
+            self.add_module('flow_' + str(k), flow_k)
+
+    def get_orthonormal(self, Q):
+        """
+        Iterative orthogonalization of Q(0) up to Q(self.steps) (in parallel for all flows)
+        shape Q = (batch_size * num_flows, z_size, M)
+        """
+        
+        # Reshape from (batch_size, num_flows * z_size * M) to (batch_size * num_flows, z_size, M)
+        Q = Q.view(-1, self.z_size , self.M)
+        norm = torch.norm(Q, p=2, dim=[1,2], keepdim=True)
+        Q_k = torch.div(Q, norm)
+            
+        # Iterative orthogonalization
+        for k in range(self.steps):
+            prod = torch.bmm(Q_k.transpose(2, 1), Q_k)
+            prod = self.identity + 0.5 * (self.identity - prod)
+            Q_k = torch.bmm(Q_k, prod)
+            
+            # Checking convergence - ||Q^T Q - I||<= self.epsilon
+            prod = torch.bmm(Q_k.transpose(2, 1), Q_k) - self.identity
+            norm = torch.sum(torch.norm(prod, p='fro', dim=2) ** 2, dim=1)
+            norm = torch.sqrt(norm)
+            max_norm = torch.max(norm)
+            if max_norm <= self.epsilon:
+                break
+        
+        if max_norm > self.epsilon:
+            print('WARNING: orthogonalization has not converged with repect to the threshold (epsilon)')
+
+        Q_k = Q_k.view(-1, self.num_flows, self.z_size, self.M)
+
+        return Q_k.transpose(1, 0)
+
+    def encode(self, x):
+        """
+        Outputs the parameters of the base distribution q_0 and the flow parameters Q,R,R_tilde, b
+        """
+
+        batch_size = x.size(0)
+
+        h = self.encoder(x)
+        h = h.view(h.size(0), -1)
+        mu = self.mu(h)
+        var = self.var(h)
+        
+        #Return amortized Q, b, R, R_tilde
+        Q = self.amor_Q(h)
+        b = self.amor_b(h)
+        R = self.amor_R(h)
+        R_tilde = self.amor_R_tilde(h)
+        
+        R = R.view(batch_size, self.M, self.M, self.num_flows)
+        R_tilde = R_tilde.view(batch_size, self.M, self.M, self.num_flows)
+
+        R = R * self.upper_triang
+        R_tilde = R_tilde * self.upper_triang       
+        
+        if self.invertibility:
+            diag1 = self.amor_diag1(h)
+            diag2 = self.amor_diag2(h)
+            diag1 = diag1.view(batch_size, self.M, self.num_flows)
+            diag2 = diag2.view(batch_size, self.M, self.num_flows)
+            R[:, range(self.M), range(self.M), :] = diag1
+            R_tilde[:, range(self.M), range(self.M), :] = diag2
+            
+        # Resize b from shape [batch_size, M * num_flows] to [batch_size, M, num_flows]
+        b = b.view(batch_size, self.M, self.num_flows)
+      
+        return mu, var, Q, R, R_tilde, b
+
+    def forward(self, x):
+
+        if self.is_cuda:
+            log_det_j = torch.zeros([x.shape[0]]).cuda()
+        else:
+            log_det_j = torch.zeros([x.shape[0]])
+
+        z_mu, z_var, Q, R, R_tilde, b = self.encode(x)
+
+        # Orthogonalize all q matrices
+        Q_orthonormal = self.get_orthonormal(Q)
+
+        # z_0
+        z = [self.reparameterize(z_mu, z_var)]
+
+        # Normalizing flows
+        for k in range(self.num_flows):
+
+            flow_k = getattr(self, 'flow_' + str(k))
+            z_k, log_det_jacobian = flow_k(z[k], Q_orthonormal[k, :, :, :], R[:, :, :, k], R_tilde[:, :, :, k], b[:, :, k])
+
+            z.append(z_k)
+            log_det_j += log_det_jacobian
+
+        x_mean = self.decode(z[-1])
+
+        return x_mean, z_mu, z_var, log_det_j, z[0], z[-1]
